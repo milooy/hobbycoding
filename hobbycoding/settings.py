7f2942ae45091d0028b3ec573c7c13c03e44e362
yoolmoo <yoolmoo@smartstudy.co.kr>
2016-02-07 01:02:26 +0900

    meetup model 생성

--- hobbycoding/settings.py
+++ hobbycoding/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'meetup',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    ("components", os.path.join(BASE_DIR, "bower_components")),
+)
--- meetup/models.py
+++ meetup/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Meetup(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    desc = models.TextField()
+    image_file = models.ImageField(upload_to='%Y/%m/%d')
+    created_date = models.DateTimeField(
+        default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
