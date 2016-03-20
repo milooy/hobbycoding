0a22546164ac9cc2f0d9f6d7361f608bb0204a8f
milooy <milooy@icloud.com>
2016-03-20 13:28:06 +0900

    add comments, tags model

--- meetup/models.py
+++ meetup/models.py
@@ -3,9 +3,16 @@ from django.db import models
 from django.utils import timezone
 
 
+# 태그
+class TagModel(models.Model):
+    title = models.CharField(max_length=20, verbose_name=u'태그')
+
+
+# 밋업
 class Meetup(models.Model):
     # 관계
     # TODO: related_name이 뭐지
+    # TODO: verbose_name과 그냥 앞에 쓴 것의 차이점
     author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=u'주최자', related_name='my_meetup')
     likes = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=u'관심', blank=True, related_name='liked_meetup')
 
@@ -13,10 +20,8 @@ class Meetup(models.Model):
     title = models.CharField(u'제목', max_length=200)
     desc = models.TextField(u'설명')
     image_file = models.ImageField(u'썸네일', upload_to='%Y/%m/%d')
-    created_date = models.DateTimeField(
-        u'생성일', default=timezone.now)
-    modified_date = models.DateTimeField(
-        u'수정일', blank=True, null=True)
+    created_date = models.DateTimeField(u'생성일', default=timezone.now)
+    modified_date = models.DateTimeField(u'수정일', blank=True, null=True)
 
     def modify(self):
         self.modified_date = timezone.now()
@@ -24,3 +29,11 @@ class Meetup(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# 댓글
+class Comments(models.Model):
+    meetup = models.ForeignKey(Meetup)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=u'이름', related_name='comments_author')
+    content = models.TextField(u'댓글', max_length=2000)
+    created_date = models.DateTimeField(u'생성일', default=timezone.now)
