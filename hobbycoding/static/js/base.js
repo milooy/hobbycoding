ab83c2411bbed9bed4ce490808579f73fa9dbded
milooy <milooy@icloud.com>
2016-05-09 02:14:30 +0900

    meetup detail 페이지 헤더 디자인(이미지, 타이틀 정렬, 태그 추가)

--- hobbycoding/static/js/base.js
+++ hobbycoding/static/js/base.js
@@ -11,7 +11,7 @@ $('.nav-main').shy({
 
 /*-- 헤더가 보일 떈 nav를 투명하게 한다 --*/
 $(window).scroll(function() {
-    if(document.body.scrollTop>=200) {
+    if(document.body.scrollTop>=180) {
         $('.nav-main').removeClass('nav-transparent')
     } else {
         $('.nav-main').addClass('nav-transparent');
