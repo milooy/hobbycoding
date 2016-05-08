d5d71163b3edadf783864ab33151a8b286564948
milooy <milooy@icloud.com>
2016-05-09 01:01:34 +0900

    헤더가 보일 땐 nav를 투명하게 한다

--- hobbycoding/static/js/base.js
+++ hobbycoding/static/js/base.js
@@ -1,3 +1,4 @@
+/*-- materializecss side bar --*/
 $('.button-collapse').sideNav();
 
 /*-- 위로 스크롤 할때 보이는 네비게이션 바 --*/
@@ -6,4 +7,13 @@ $('.nav-main').shy({
     thresholdDown: 5,
     animationDelay: 0,
     animationDur: 150
+});
+
+/*-- 헤더가 보일 떈 nav를 투명하게 한다 --*/
+$(window).scroll(function() {
+    if(document.body.scrollTop>=200) {
+        $('.nav-main').removeClass('nav-transparent')
+    } else {
+        $('.nav-main').addClass('nav-transparent');
+    }
 });
