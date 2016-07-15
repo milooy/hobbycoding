e846ff32631ad71f8a684901f12c7e6c2a7d2251
milooy <milooy@icloud.com>
2016-07-15 18:55:02 +0900

    crop을 가운데부터 하도록(python 3 호환)

--- meetup/forms.py
+++ meetup/forms.py
@@ -31,17 +31,18 @@ class MeetupEditForm(forms.ModelForm):
         max_width = width
         max_height = height
 
-        # input_file = StringIO(data)
         input_file = BytesIO(data.read())
         img = pil.open(input_file)
         if not force:
             img.thumbnail((max_width, max_height), pil.ANTIALIAS)
         else:
             src_width, src_height = img.size
-            src_ratio = float(src_width) // float(src_height)
+            src_ratio = float(src_width) / float(src_height)
             dst_width, dst_height = max_width, max_height
-            dst_ratio = float(dst_width) // float(dst_height)
+            dst_ratio = float(dst_width) / float(dst_height)
 
+            print(src_width, src_height)
+            print(src_ratio, dst_ratio)
             if dst_ratio < src_ratio:
                 crop_height = src_height
                 crop_width = crop_height * dst_ratio
@@ -49,31 +50,18 @@ class MeetupEditForm(forms.ModelForm):
                 y_offset = 0
             else:
                 crop_width = src_width
-                crop_height = crop_width // dst_ratio
+                crop_height = crop_width / dst_ratio
                 x_offset = 0
                 y_offset = int(src_height - crop_height) // 3
             img = img.crop((x_offset, y_offset, x_offset+int(crop_width), y_offset+int(crop_height)))
             img = img.resize((dst_width, dst_height), pil.ANTIALIAS)
-
-        # tmp = BytesIO()
-        # img.save(tmp, 'JPEG')
-        # tmp.seek(0)
-        # output_data = tmp.getvalue()
-        # input_file.close()
-        # tmp.close()
-        #
-        # return output_data
 
         image_file = BytesIO()
         img.save(image_file, 'JPEG')
         data.file = image_file
         return data
 
-
-
     def save(self, commit=True):
-        print('이거 불리냐')
-        print(self.cleaned_data.get('image_file'))
         instance = super(MeetupEditForm, self).save(commit=False)
         instance.author = self.request.user
         instance.published_date = timezone.now()
